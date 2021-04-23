a11c531b33af28dbac90f3ebe2ac74debcd9d9e4
Jordi Noguera <jordinoguera83+github@gmail.com>
2021-04-23 13:54:13 +0200

    Add npm package used in function

--- some/directory/with/functions/hello.js
+++ some/directory/with/functions/hello.js
@@ -1,8 +1,9 @@
 require('../../other')
+const lodash = require('lodash')
 
 exports.handler = async function(event, context) {
   return {
     statusCode: 200,
-    body: JSON.stringify({message: "Hello World"})
+    body: JSON.stringify({message: `Hello World ${lodash.random()}`})
   };
 }
